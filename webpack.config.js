af1d8be72713861b7ce4ebd8584972385a72a0b0
오민형 <minhyung@ridi.com>
2020-02-04 18:49:28 +0900

    webpack 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const { getDefinitions } = require('./env/publicRuntimeConfig');
 
