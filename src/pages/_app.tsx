57a6dc6c81d34f1db182d97b16d8befe1ed4aa31
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-06-09 16:09:19 +0900

    모든 환경에 AccountProvider 추가
    (cherry picked from commit 9de27924c10d6bc4a4505d0da2245632ceb906b0)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -145,7 +145,9 @@ class StoreApp extends App<StoreAppProps, StoreAppState> {
           </PartialSeparator>
           {/* Todo Apply Layout */}
           <Provider store={store}>
-            <Component {...pageProps} />
+            <AccountProvider>
+              <Component {...pageProps} />
+            </AccountProvider>
           </Provider>
         </>
       );
@@ -159,13 +161,15 @@ class StoreApp extends App<StoreAppProps, StoreAppState> {
           <CacheProvider value={createCache({ ...cache, nonce })}>
             <Global styles={resetStyles} />
             <Provider store={store}>
-              <InAppThemeProvider theme={theme ?? ''}>
-                <ViewportIntersectionProvider>
-                  <Contents>
-                    <Component {...pageProps} />
-                  </Contents>
-                </ViewportIntersectionProvider>
-              </InAppThemeProvider>
+              <AccountProvider>
+                <InAppThemeProvider theme={theme ?? ''}>
+                  <ViewportIntersectionProvider>
+                    <Contents>
+                      <Component {...pageProps} />
+                    </Contents>
+                  </ViewportIntersectionProvider>
+                </InAppThemeProvider>
+              </AccountProvider>
             </Provider>
           </CacheProvider>
         </>
