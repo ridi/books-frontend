2e2458ec34414464527ba7a2d793f8e3c6812699
Heejae Jang <j132134@gmail.com>
2020-07-31 12:12:32 +0900

    Added `window.isLoginRequired`

--- src/pages/inapp/search.tsx
+++ src/pages/inapp/search.tsx
@@ -5,8 +5,10 @@ import SearchPage from 'src/pages/search';
 export default () => {
   React.useEffect(() => {
     window.isExternalLink = (url: string) => !/(\/inapp\/search\?|\/books\/[0-9]+\/in-app-search\?|\?q=)/g.test(url);
+    window.isLoginRequired = () => false;
     return () => {
       delete window.isExternalLink;
+      delete window.isLoginRequired;
     };
   }, []);
 
