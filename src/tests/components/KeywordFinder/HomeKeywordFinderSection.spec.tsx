ba642494b5140a01dfb2c006bdb436dc45a7c6e9
Heejae Jang <j132134@gmail.com>
2020-10-16 14:29:56 +0900

    Fix: test codes

--- src/tests/components/KeywordFinder/HomeKeywordFinderSection.spec.tsx
+++ src/tests/components/KeywordFinder/HomeKeywordFinderSection.spec.tsx
@@ -7,11 +7,102 @@ import { defaultTheme } from 'src/styles';
 
 afterEach(cleanup);
 
+interface Keyword {
+  id: number;
+  name: string;
+  tag_id: number;
+  set_id: number;
+}
+
+const popularKeywords: Record<string, Keyword[]> = {
+  'bl-novel': [
+    {
+      id: 1,
+      set_id: 15,
+      tag_id: 2412,
+      name: '판타지물',
+    },
+    {
+      id: 1,
+      set_id: 15,
+      tag_id: 2414,
+      name: '동양풍',
+    },
+    {
+      id: 1,
+      set_id: 15,
+      tag_id: 2806,
+      name: '차원이동/영혼바뀜',
+    },
+  ],
+  comics: [
+    {
+      id: 1,
+      set_id: 20,
+      tag_id: 188,
+      name: '러브코믹',
+    },
+    {
+      id: 1,
+      set_id: 20,
+      tag_id: 2972,
+      name: '츤데레',
+    },
+    {
+      id: 1,
+      set_id: 16,
+      tag_id: 555,
+      name: '달달물',
+    },
+  ],
+  'fantasy-serial': [
+    {
+      id: 1,
+      set_id: 18,
+      tag_id: 92,
+      name: '현대판타지',
+    },
+    {
+      id: 1,
+      set_id: 18,
+      tag_id: 96,
+      name: '차원이동물',
+    },
+    {
+      id: 1,
+      set_id: 18,
+      tag_id: 88,
+      name: '게임판타지',
+    },
+  ],
+  romance: [
+    {
+      id: 1,
+      set_id: 1,
+      tag_id: 150,
+      name: '고수위',
+    },
+    {
+      id: 1,
+      set_id: 1,
+      tag_id: 52,
+      name: '후회남',
+    },
+    {
+      id: 1,
+      set_id: 1,
+      tag_id: 36,
+      name: '친구>연인',
+    },
+  ],
+};
+
 const renderComponent = (genre: string) =>
   render(
     <ThemeProvider theme={defaultTheme}>
       <HomeKeywordFinderSection
         genre={genre}
+        items={popularKeywords[genre]}
       />
     </ThemeProvider>,
   );
@@ -23,7 +114,7 @@ describe('test keyword finder section', () => {
     expect(itemNode).not.toBe(null);
   });
   it('should be render fantasy keyword', () => {
-    const { container } = renderComponent('fantasy');
+    const { container } = renderComponent('fantasy-serial');
     const itemNode = getByText(container, '게임판타지');
     expect(itemNode).not.toBe(null);
   });
