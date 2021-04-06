5a04d194f16a7061c7e90a9e301b2217993b0aca
JB Lee <jbl@ip-10-10-1-202.ap-northeast-2.compute.internal>
2021-04-06 12:04:58 +0900

    마케팅 Hotjar 스크립트 인라인 방식으로 변경(테스트용)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -99,12 +99,22 @@ class StoreApp extends App<StoreAppProps> {
     }
     return (
       <script
-        id="hotjar-init"
-        data-hjid={hjid}
-
         async
         nonce={this.props.nonce}
-        src="/hotjar-init.js"
+
+        id="hotjar-init"
+        data-hjid={hjid}
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            hjid=document.getElementById('hotjar-init') && document.getElementById('hotjar-init').getAttribute('data-hjid');
+            h._hjSettings={hjid:hjid,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
       />
     );
   }
